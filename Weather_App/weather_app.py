3764aa3633331d6d7ff542514d7392d876107e57
Sreeja Sreekumar Sujatha <sreejasreekumarsujatha@gmail.com>
2025-04-24 22:59:52 -0400

    Updated the canvas with weather img on the title

--- Weather_App/weather_app.py
+++ Weather_App/weather_app.py
@@ -24,6 +24,21 @@ def create_gradient(canvas, width, height):
         g = int(start_color[1] + (end_color[1] - start_color[1]) * i / height)
         b = int(start_color[2] + (end_color[2] - start_color[2]) * i / height)
         canvas.create_line(0, i, width, i, fill=f"#{r:02x}{g:02x}{b:02x}", width=1)
+        
+        
+def draw_heading(event):
+    width = canvas.winfo_width()
+    canvas.delete("heading")
+    img_width = 90
+    total_width = img_width + width
+    canvas.create_image(total_width / 2 + 160, 110, tags="heading")
+    canvas.create_text(width / 2 + 2, 72, text="Weather App",
+                       fill="black", font=("Comic Sans MS", 30, "bold"), tags="heading")
+    canvas.create_text(width / 2, 70,
+                       text="Weather App",
+                       fill="white",
+                       font=("Comic Sans MS", 30, "bold"),
+                       tags="heading")
 
 
 # Initialize the main window
@@ -41,3 +56,9 @@ canvas = tk.Canvas(window, width=900, height=700)
 canvas.pack(fill="both", expand=True)
 
 create_gradient(canvas, 900, 700)
+
+# Bind window resize to heading redraw
+window.bind("<Configure>", draw_heading)
+
+# Start app loop
+window.mainloop()
