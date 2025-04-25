736770810c75804bc83c9c3d9afe1657c4fdb624
Sreeja Sreekumar Sujatha <sreejasreekumarsujatha@gmail.com>
2025-04-24 22:36:49 -0400

    Weather app using tkinter started

---
+++ Weather_App/weather_app.py
@@ -0,0 +1,16 @@
+# Creating a small weather app using tkinter
+# Importing tkinter from the tkinter module and give tk as alias name  to create the GUI
+# Tk create main window, layout management, and some widgets that ttk doesn’t have, provides modern-looking widgets
+# From tkinter module importing ttk(submodule) is used to generate advanced widget
+# Import requests module to get data from api,send data to server
+# From PIL module import Image,Imagetk module for loading  and displaying images
+# Import pyttsx3 used for converting text to speech
+
+# Creating a small weather app using tkinter
+
+import tkinter as tk
+from tkinter import PhotoImage, messagebox
+import customtkinter as ctk
+import requests
+from PIL import Image, ImageTk
+import pyttsx3
